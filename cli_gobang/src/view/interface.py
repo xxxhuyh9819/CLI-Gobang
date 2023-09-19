bed55eb0fd71f65a587eb8a757a5abd397e6d70d
yunhao <xxxhuyh9819@gmail.com>
2023-09-19 16:35:00 -0500

    Changed the render() function to show row and column numbers

--- cli_gobang/src/view/interface.py
+++ cli_gobang/src/view/interface.py
@@ -1,9 +1,23 @@
 class Panel(object):
 
     def render(self, board):
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                print(str(board[i][j]), end='  ')
+        print(end=" ")
+        for i in range(len(board) + 1):
+            for j in range(len(board[0]) + 1):
+                if i == 0 and j != len(board[0]):
+                    if j < 10:
+                        print("   " + f"{j}", end='')
+                    else:
+                        print("  " + f"{j}", end='')
+                    continue
+                if j == 0 and i > 0:
+                    if i <= 10:
+                        print(f"{i - 1}", end='  ')
+                    else:
+                        print(f"{i - 1}", end=' ')
+                if j == len(board[0]):
+                    continue
+                print(" " + str(board[i - 1][j - 1]), end='  ')
             print()
 
     def startPrompt(self):
